e89b74ed159cda58d0f70b7a758864d7fdedaddf
Bayram Çınar <bayram200212@gmail.com>
2023-12-08 23:03:56 +0300

    third commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "../style/footer.css"
 
 function Footer() {
   return (
-    <div className='bg-purpleOne'>
+    <div className='footer'>
       <h1 className='text-white text-xl text-center p-3 font-medium'>© Copyright 2023-2024 Tüm Hakları Saklıdır. Created by @cukurovablockchain.</h1>
     </div>
   )
